85fec09df3e5516f685e9e0ce4fe499b9e63cb4a
Henry <henry.23.tc@gmail.com>
2023-07-17 19:55:54 -0500

    Consulta de los datos al usuario

---
+++ Combustible.py
@@ -0,0 +1,7 @@
+def main():
+    # Primero consultamos los datos al usuario
+    kilometros_recorridos = float(input("Ingrese la cantidad de kilómetros recorridos por la motocicleta: "))
+    litros_combustible = float(input("Ingrese la cantidad de litros de combustible consumidos: "))
+
+if __name__ == "__main__":
+    main()
