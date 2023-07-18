6f3ff5926302fe9a2221f7cc73aaaff56d0044b9
iber <iberson-silva@outlook.com>
2023-07-17 20:00:35 -0500

    implementación de algoritmo y resultado

--- Combustible.py
+++ Combustible.py
@@ -3,5 +3,11 @@ def main():
     kilometros_recorridos = float(input("Ingrese la cantidad de kilómetros recorridos por la motocicleta: "))
     litros_combustible = float(input("Ingrese la cantidad de litros de combustible consumidos: "))
 
+    # Luego calculamos el consumo de combustible en kilómetros por litro
+    consumo_kilometros_litro = kilometros_recorridos / litros_combustible
+
+    # Por último mostramos el resultado
+    print(f"El consumo de combustible es de {consumo_kilometros_litro:.2f} kilómetros por litro.")
+
 if __name__ == "__main__":
     main()
