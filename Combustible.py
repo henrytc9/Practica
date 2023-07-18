b24bc0ba4373e032fdef29289ea67ac9d43d2292
iber <iberson-silva@outlook.com>
2023-07-17 20:15:31 -0500

    modificar consultas de datos

--- Combustible.py
+++ Combustible.py
@@ -12,9 +12,9 @@ def obtener_numero_positivo(mensaje):
 
 def main():
     # Primero consultamos los datos al usuario
-    kilometros_recorridos = float(input("Ingrese la cantidad de kilómetros recorridos por la motocicleta: "))
-    litros_combustible = float(input("Ingrese la cantidad de litros de combustible consumidos: "))
-
+    kilometros_recorridos = obtener_numero_positivo("Ingrese la cantidad de kilómetros recorridos por la motocicleta: ")
+    litros_combustible = obtener_numero_positivo("Ingrese la cantidad de litros de combustible consumidos: ")
+    
     # Luego calculamos el consumo de combustible en kilómetros por litro
     consumo_kilometros_litro = kilometros_recorridos / litros_combustible
 
